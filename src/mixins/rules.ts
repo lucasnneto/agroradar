3e74ad28a20e1f6a58a966bf004a612a36aa5c5b
Lucas Neto <lucasn-oliveira@hotmail.com>
2021-09-25 15:00:06 -0300

    login and register

--- src/mixins/rules.ts
+++ src/mixins/rules.ts
@@ -1,9 +1,57 @@
+import { removerMask } from "@/mixins/utils";
+export function validCpf(cpf: string) {
+  if (
+    !cpf ||
+    cpf.length !== 11 ||
+    cpf === "00000000000" ||
+    cpf === "11111111111" ||
+    cpf === "22222222222" ||
+    cpf === "33333333333" ||
+    cpf === "44444444444" ||
+    cpf === "55555555555" ||
+    cpf === "66666666666" ||
+    cpf === "77777777777" ||
+    cpf === "88888888888" ||
+    cpf === "99999999999"
+  ) {
+    return false;
+  }
+  let soma = 0;
+  let resto;
+  for (let i = 1; i <= 9; i++) {
+    soma = soma + parseInt(cpf.substring(i - 1, i), 10) * (11 - i);
+  }
+  resto = (soma * 10) % 11;
+  if (resto === 10 || resto === 11) {
+    resto = 0;
+  }
+  if (resto !== parseInt(cpf.substring(9, 10), 10)) {
+    return false;
+  }
+  soma = 0;
+  for (let i = 1; i <= 10; i++) {
+    soma = soma + parseInt(cpf.substring(i - 1, i), 10) * (12 - i);
+  }
+  resto = (soma * 10) % 11;
+  if (resto === 10 || resto === 11) {
+    resto = 0;
+  }
+  if (resto !== parseInt(cpf.substring(10, 11), 10)) {
+    return false;
+  }
+  return true;
+}
+
 export default {
   data() {
     return {
       rules: {
         required: (value: string) => !!value || "Requerido.",
+        cpf: (value: string) => {
+          // const pattern = /^([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})$/;
 
+          return value ? validCpf(removerMask(value)) || "CPF inválido" : true;
+        },
         email: (value: string) => {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
           return pattern.test(value) || "E-mail inválido.";
--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,11 +1,14 @@
 import { axiosCall } from "@/service/axios";
 import router from "@/router";
+import Vue from "vue";
 interface StadeBase {
   token: string;
+  status: string;
 }
 const getDefaultState = () => {
   return {
     token: "",
+    status: "",
   };
 };
 const state = getDefaultState();
@@ -27,18 +30,46 @@ const actions = {
   CHANGE({ commit }: any, payload: StadeBase): void {
     commit("CHANGE", payload);
   },
-  async LOGIN({ commit }: any, payload: any): Promise<void> {
+  async LOGIN({ commit, state }: any, payload: any): Promise<void> {
+    if (state.status === "loading") return;
+    commit("CHANGE", { status: "loading" });
     const [error, data] = await axiosCall({
       method: "post",
       url: "/authenticate",
       data: payload,
     });
     if (error) {
-      console.log(error);
+      console.log("error", error);
+      commit("CHANGE", { status: "error" });
+      if (error?.message) {
+        Vue.$toast.error(error.message);
+      } else {
+        Vue.$toast.error("Ocorreu um erro interno!");
+      }
     } else {
-      commit("CHANGE", { token: data.token });
+      commit("CHANGE", { token: data.token, status: "" });
       router.push({ name: "dashboard" });
-      console.log(data);
+    }
+  },
+  async REGISTRAR({ commit, state }: any, payload: any): Promise<void> {
+    if (state.status === "loading") return;
+    commit("CHANGE", { status: "loading" });
+    const [error, data] = await axiosCall({
+      method: "post",
+      url: "/users/register",
+      data: payload,
+    });
+    if (error) {
+      console.log("error", error);
+      commit("CHANGE", { status: "error" });
+      if (error?.message) {
+        Vue.$toast.error(error.message);
+      } else {
+        Vue.$toast.error("Ocorreu um erro interno!");
+      }
+    } else {
+      commit("CHANGE", { token: data.token, status: "" });
+      router.push({ name: "dashboard" });
     }
   },
 };
